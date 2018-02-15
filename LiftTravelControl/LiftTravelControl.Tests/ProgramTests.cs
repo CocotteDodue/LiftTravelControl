84e2b1ca4349de54d5e6c52af8611072d93078eb
Mathilde <ceccaroli.m@gmail.com>
2018-02-16 07:42:15 +1000

    [#3] Default program parameters inputs

--- LiftTravelControl/LiftTravelControl.Tests/ProgramTests.cs
+++ LiftTravelControl/LiftTravelControl.Tests/ProgramTests.cs
@@ -1,3 +1,4 @@
+
 using System;
 using Xunit;
 
@@ -6,9 +7,27 @@ namespace LiftTravelControl.Tests
     public class ProgramTests
     {
         [Fact]
-+        public void Program_MustThrowExecption_WhenCurrentParkedFloorValueNotRecievedAtStart()
-+        {
-+            Assert.Throws<ArgumentException>(() => Program.Main(null));
-+        }
-}
+        public void Program_MustThrowExecption_WhenCurrentParkedFloorValueNotRecievedAtStart()
+        {
+            Assert.Throws<ArgumentException>(() => Program.Main(null));
+        }
+
+        [Theory]
+        [InlineData("Foo")]
+        [InlineData("7.5")]
+        [InlineData("")]
+        public void Program_MustThrowExecption_WhenCurrentParkedFloorValueParameterNotInteger(string testValue)
+        {
+            Assert.Throws<ArgumentException>(() => Program.Main(new[] { testValue }));
+        }
+
+        [Theory]
+        [InlineData("-2")]
+        [InlineData("11")]
+        [InlineData("145")]
+        public void Program_MustThrowExecption_WhenCurrentParkedFloorValueParameterNotIn1To10Range(string testValue)
+        {
+            Assert.Throws<ArgumentException>(() => Program.Main(new[] { testValue }));
+        }
+    }
 }
