0376423d692dd688d75621a04b6a3b09c905986f
Mathilde <ceccaroli.m@gmail.com>
2018-02-16 17:22:11 +1000

    [#21] 2 users traveling in opposite directions

--- LiftTravelControl/LiftTravelControl.Tests/LiftTests/LiftTests.TwoUsersScenario.cs
+++ LiftTravelControl/LiftTravelControl.Tests/LiftTests/LiftTests.TwoUsersScenario.cs
@@ -373,6 +373,33 @@ namespace LiftTravelControl.Tests.LiftTests
             Assert.Equal(request1.SummonFloor, planResult.ElementAt(3));
         }
 
+        [Fact]
+        public void ExecutionPlan_HasSummonAndDesitnationAscOrder_WhenSummonedFrom2DifferentFloorsAndFirstSummonRequestUpAndSecondSummonRequestDown()
+        {
+            FloorConfiguration floorConfig = new FloorConfiguration(6, 0, 15);
+            IExecutionPlan plan = new ExecutionPlan();
+            ILift lift = new Lift(floorConfig, plan);
+            SummonInformation summon1 = new SummonInformation(6, TravelDirection.Up);
+            SummonInformation request1 = new SummonInformation(13, TravelDirection.None);
+            SummonInformation summon2 = new SummonInformation(5, TravelDirection.Down);
+            SummonInformation request2 = new SummonInformation(1, TravelDirection.None);
+            IList<SummonInformation> requests = new List<SummonInformation>()
+            {
+                summon1,
+                request1,
+                summon2,
+                request2
+            };
+
+            var executionPlan = lift.ProcessRequests(requests);
+
+            var planResult = executionPlan.GetFloorVisitationPlan();
+            Assert.Equal(summon1.SummonFloor, planResult.ElementAt(0));
+            Assert.Equal(request1.SummonFloor, planResult.ElementAt(1));
+            Assert.Equal(summon2.SummonFloor, planResult.ElementAt(2));
+            Assert.Equal(request2.SummonFloor, planResult.ElementAt(3));
+        }
+
         #endregion
     }
 }
