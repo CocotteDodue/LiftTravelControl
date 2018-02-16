475c57eca809ab9add4781bb3fef188208084186
Mathilde <ceccaroli.m@gmail.com>
2018-02-16 17:17:19 +1000

    [#17] 2 users - from different floors and different floors than parked to floors lower than parked

--- LiftTravelControl/LiftTravelControl.Tests/LiftTests/LiftTests.TwoUsersScenario.cs
+++ LiftTravelControl/LiftTravelControl.Tests/LiftTests/LiftTests.TwoUsersScenario.cs
@@ -346,6 +346,32 @@ namespace LiftTravelControl.Tests.LiftTests
             Assert.Equal(summon2.SummonFloor, planResult.ElementAt(2));
             Assert.Equal(request2.SummonFloor, planResult.ElementAt(3));
         }
+        [Fact]
+        public void ExecutionPlan_HasSummonAndDesitnationAscOrder_WhenSummonedFrom2DifferentFloorsLowerThanParkedToUpAndDifferentRequestedFloorsAndUser2RequestLowerFloorThanUser1()
+        {
+            FloorConfiguration floorConfig = new FloorConfiguration(8, 0, 15);
+            IExecutionPlan plan = new ExecutionPlan();
+            ILift lift = new Lift(floorConfig, plan);
+            SummonInformation summon1 = new SummonInformation(6, TravelDirection.Up);
+            SummonInformation request1 = new SummonInformation(13, TravelDirection.None);
+            SummonInformation summon2 = new SummonInformation(4, TravelDirection.Up);
+            SummonInformation request2 = new SummonInformation(12, TravelDirection.None);
+            IList<SummonInformation> requests = new List<SummonInformation>()
+            {
+                summon1,
+                request1,
+                summon2,
+                request2
+            };
+
+            var executionPlan = lift.ProcessRequests(requests);
+
+            var planResult = executionPlan.GetFloorVisitationPlan();
+            Assert.Equal(summon2.SummonFloor, planResult.ElementAt(0));
+            Assert.Equal(summon1.SummonFloor, planResult.ElementAt(1));
+            Assert.Equal(request2.SummonFloor, planResult.ElementAt(2));
+            Assert.Equal(request1.SummonFloor, planResult.ElementAt(3));
+        }
 
         #endregion
     }
